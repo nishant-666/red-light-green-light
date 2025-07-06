d82454703564b25bdb19282d6262d504864a811f
nishant-666 <nishants440@gmail.com>
2025-07-06 22:37:06 +0530

    Final

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const gameArea = document.getElementById("game-area");
   const startBtn = document.getElementById("start-btn");
+  const resetBtn = document.getElementById("reset-btn");
 
   const dollImage = document.createElement("img");
   const playerImage = document.createElement("img");
   const finishLine = document.createElement("div");
+
+  const gameAudio = document.createElement("audio");
+  gameAudio.src = "./assets/doll.mp3";
 
   finishLine.style.backgroundColor = "white";
   finishLine.style.width = "700px";
@@ -24,25 +28,29 @@ document.addEventListener("DOMContentLoaded", () => {
   let playerPosition = 10;
   let isWatching = true;
 
-  startBtn.addEventListener("click", () => {
-    const gameAudio = document.createElement("audio");
+  const linePostion = 600;
+  let gameOver = false;
 
+  let interval;
+
+  startBtn.addEventListener("click", () => {
     const forwardFace = Math.floor(Math.random() * 5) + 2;
     const backwardFace = 4000;
 
-    setTimeout(() => {
+    startBtn.disabled = true;
+
+    interval = setInterval(() => {
       dollImage.src = "./assets/doll-back.png";
       isWatching = false;
 
       setTimeout(() => {
         dollImage.src = "./assets/doll-front.png";
+        gameAudio.currentTime = 0;
+
+        gameAudio.play();
         isWatching = true;
       }, backwardFace);
     }, forwardFace * 1000);
-
-    console.log(forwardFace);
-
-    gameAudio.src = "./assets/doll.mp3";
 
     playerImage.src = "./assets/player.png";
 
@@ -56,13 +64,49 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   document.addEventListener("keydown", (event) => {
+    if (gameOver) return;
+
     if (event.code === "ArrowUp") {
       if (isWatching) {
+        gameOver = true;
+        clearInterval(interval);
         alert("Game Over!");
+
+        startBtn.disabled = false;
       } else {
+        if (playerPosition >= linePostion) {
+          gameOver = true;
+          clearInterval(interval);
+          alert("You Win!");
+
+          startBtn.disabled = false;
+        }
         playerPosition = playerPosition + 10;
         playerImage.style.bottom = `${playerPosition}px`;
       }
     }
   });
+
+  resetBtn.addEventListener("click", () => {
+    resetGame();
+  });
+
+  function resetGame() {
+    clearInterval(interval);
+    gameAudio.pause();
+    gameAudio.currentTime = 0;
+    playerPosition = 10;
+    gameOver = false;
+    isWatching = true;
+
+    if (playerImage.parentNode) {
+      playerImage.remove();
+    }
+
+    dollImage.src = "./assets/doll-front.png";
+    playerImage.style.bottom = `${playerPosition}px`;
+
+    startBtn.disabled = false;
+    resetBtn.disabled = true;
+  }
 });
